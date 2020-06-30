cae7c24cd5fd2c24ea0bb8f54807c6fae8819686
Get-sunshine <1689103894@qq.com>
2020-06-30 09:33:19 +0800

    分页功能的实现

--- server/src/routes/account.js
+++ server/src/routes/account.js
@@ -1,13 +1,15 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
 const conn = require('../db/conn');
+// 前缀
+router.prefix('/account');
 // 添加账号
-router.post('/account/accountadd', async (ctx, next) => {
+router.post('/accountadd', async (ctx, next) => {
   let { account, password, userType } = ctx.request.body;
   // SQL语句
   let sqlStr = `insert into account (account,password,user_type) values
                 ('${account}','${password}','${userType}');`;
   // 执行SQL语句
-  await conn.query(sqlStr).then((res)=> {
+  await conn.query(sqlStr).then((res) => {
     if (res.affectedRows !== 0) { // 成功
       ctx.response.body = {
         code: 0,
@@ -26,40 +28,123 @@ router.post('/account/accountadd', async (ctx, next) => {
     });
 });
 // 获取账号列表
-router.get('/account/accountlist',async (ctx,nect) => {
+router.get('/accountlist', async (ctx, nect) => {
+  let { currentPage, pageSize } = ctx.request.query;
   // sql语句
   let sqlStr = `select id,account,password,user_type as userType,create_date as createDate 
-  from account order by id desc;`;
+  from account order by id desc`;
   // 执行
-  await conn.query(sqlStr).then((res) => {
-    ctx.body={
-      code:0,
-      message:'查询成功',
-      res
-    };
-  })
-  .catch((err) => {
+  await conn.query(sqlStr).then(async (res) => {
+    // 总条数
+    let userTotal = res.length;
+    // 略过条数
+    let skipAmount = (currentPage - 1) * pageSize;
+    // 重新拼接SQL
+    sqlStr += ` limit ${skipAmount},${pageSize};`;
+    await conn.query(sqlStr).then((res) => {
+      if (res.length !== 0) {
+        ctx.body = {
+          code: 0,
+          message: '查询成功',
+          result:{
+            data:res,
+            userTotal
+          }
+        };
+        return;
+      }
+      ctx.body = {
+        code: 1,
+        message: '查询失败',
+        result: ''
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
     console.log(err);
   });
 });
-// 单个删除账号
-router.get('/account/accountdelete',async (ctx,next) => {
-  let {id} = ctx.request.query; // 获取数据
+// 删除单个账号
+router.get('/accountdelete', async (ctx, next) => {
+  let { id } = ctx.request.query; // 获取数据
   let sqlStr = `delete from account where id = ${id};`; // SQL语句
   await conn.query(sqlStr).then((res) => {
-    if(res.affectedRows!==0){
-      ctx.body={
-        code:0,
-        message:'删除成功'
+    if (res.affectedRows !== 0) {
+      ctx.body = {
+        code: 0,
+        message: '删除成功'
       }
       return;
     }
-    ctx.body={
-      code:1,
-      message:'删除失败'
+    ctx.body = {
+      code: 1,
+      message: '删除失败'
     }
     return;
   });
 
+});
+// 修改数据-数据回填模态框
+router.get('/accountedit', async (ctx, next) => {
+  let { id } = ctx.request.query;
+  let sqlStr = `select id,account,user_type as userType from account 
+                where id=${id};`;
+  await conn.query(sqlStr).then((res) => {
+    ctx.body = {
+      code: 0,
+      message: '查询成功',
+      res: res[0]
+    }
+  }).catch((err) => {
+    console.log(err);
+  });
+});
+// 修改数据-数据保存到数据库
+router.post('/accounteditsave', async (ctx, next) => {
+  let { account, id, userType } = ctx.request.body;
+  // 构造SQL语句
+  let sqlStr = `update account set account='${account}',user_type='${userType}'
+  where id='${id}'`;
+  await conn.query(sqlStr).then((res) => {
+    // 成功
+    if (res.affectedRows !== 0) {
+      ctx.body = {
+        code: 0,
+        message: '修改成功',
+        res: ''
+      }
+      return;
+    }
+    ctx.body = {
+      code: 1,
+      message: '修改失败',
+      res: ''
+    }
+  });
+})
+// 批量删除
+router.post('/accountmultipledelete', async (ctx, next) => {
+  let { idArray } = ctx.request.body;
+  // SQL语句
+  let sqlStr = `delete from account where id in (${idArray});`;
+  // 执行
+  await conn.query(sqlStr).then((res) => {
+    if (res.affectedRows !== 0) {
+      ctx.body = {
+        code: 0,
+        message: '批量删除成功',
+        res: ''
+      }
+      return;
+    }
+    ctx.body = {
+      code: 1,
+      message: '批量删除失败',
+      res: ''
+    }
+  }).catch((err) => {
+    console.log(err);
+  });
 });
 module.exports = router;
--- supermarket/src/views/AccountManage/AccountManage.vue
+++ supermarket/src/views/AccountManage/AccountManage.vue
@@ -6,10 +6,11 @@
       </div>
       <div class="text item">
         <el-table
-          ref="multipleTable"
+          ref="accountManageTable"
           :data="accountManageData"
           tooltip-effect="dark"
           style="width: 100%"
+          @selection-change="handleSelectionChange"
         >
           <el-table-column type="selection" width="55"></el-table-column>
           <el-table-column prop="account" label="账号"></el-table-column>
@@ -33,23 +34,42 @@
           <el-pagination
             @size-change="handleSizeChange"
             @current-change="handleCurrentChange"
-            :current-page="1"
-            :page-sizes="[1, 3, 5, 10]"
-            :page-size="100"
+            :current-page="currentPage"
+            :page-sizes="[3, 5, 10]"
+            :page-size="pageSize"
             layout="total, sizes, prev, pager, next, jumper"
-            :total="11"
+            :total="userTotal"
           ></el-pagination>
         </div>
+        <!-- 按钮 -->
         <div style="margin-top: 20px">
-          <el-button type="primary">
+          <el-button type="primary" @click="multipleDelete">
             <i class="el-icon-delete-solid"></i>批量删除
           </el-button>
-          <el-button type="success">
+          <el-button type="success" @click="toggleSelection">
             <i class="el-icon-close"></i>取消选择
           </el-button>
         </div>
       </div>
     </el-card>
+    <!-- 模态框 -->
+    <el-dialog size="mini" width="30%" title="修改信息" :visible.sync="dialogFormVisible">
+      <el-form :model="editForm" :rules="rules" ref="editForm" status-icon>
+        <el-form-item prop="account" label="账户" :label-width="formLabelWidth">
+          <el-input v-model="editForm.account" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item prop="userType" label="用户类型" :label-width="formLabelWidth">
+          <el-select v-model="editForm.userType" placeholder="请选择用户类型">
+            <el-option label="普通管理员" value="普通管理员"></el-option>
+            <el-option label="超级管理员" value="超级管理员"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editFormSave('editForm')">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <style lang="less">
@@ -60,18 +80,77 @@ import moment from "moment";
 export default {
   data() {
     return {
-      accountManageData: []
+      // 表格数据
+      accountManageData: [],
+      // 选中项的id
+      selectedIdArray: [],
+      // 分页相关
+      currentPage: 1, // 默认第一页
+      pageSize: 3, // 默认每页3条
+      userTotal: 100, // 默认0
+      // editForm相关
+      editForm: {
+        account: "",
+        userType: ""
+      },
+      // 验证表单数据
+      rules: {
+        account: [
+          { required: true, message: "请输入", trigger: "change" },
+          { min: 3, max: 6, message: "长度 3~6 位", trigger: "blur" }
+        ],
+        userType: [
+          { required: true, message: "请选择用户身份", trigger: "change" }
+        ]
+      },
+      // 控制表单可见性
+      dialogFormVisible: false,
+      // editForm label宽度
+      formLabelWidth: "100px"
     };
   },
   methods: {
     // 获取账号列表
     getAccountList() {
+      let params = {
+        pageSize: this.pageSize,
+        currentPage: this.currentPage
+      };
       this.$http
-        .get("account/accountlist")
-        .then(res => {
+        .get("account/accountlist", params)
+        .then(response => {
           // 获取数据
-          let data = res.data.res;
-          this.accountManageData = data;
+          let { code, message, result } = response.data;
+          if (code === 0) {
+            // 总条数
+            this.userTotal = result.userTotal;
+            // user数据
+            this.accountManageData = result.data;
+            // 不是第一页 且该页用户删除完毕
+            // if (result.data.length === 1 && this.currentPage > 1) {
+            //   // 回到前一页
+            //   this.currentPage -= 1;
+            //   // 重拿数据
+            //   this.getAccountList();
+            // }
+            return;
+          }
+          // 此页没有数据，是第一页
+          if (this.currentPage === 1) {
+            this.$message({
+              type: "info",
+              message: "暂时没有数据"
+            });
+            // 总条数，没有
+            this.userTotal = result.userTotal;
+            // user数据，没有
+            this.accountManageData = result.data;
+            return;
+          }
+          // 此页没有数据，不是第一页，回到上一页
+          this.currentPage -= 1;
+          // 重拿数据
+          this.getAccountList();
         })
         .catch(err => {
           console.log(err);
@@ -79,36 +158,171 @@ export default {
     },
     //编辑
     handleEdit(id) {
-      console.log(id);
+      this.$http
+        .get("account/accountedit", { id })
+        .then(res => {
+          this.editForm.account = res.data.res.account;
+          this.editForm.userType = res.data.res.userType;
+          this.editForm["id"] = id;
+          // 显示模态框
+          this.dialogFormVisible = true;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
+    // 保存编辑
+    editFormSave(formName) {
+      this.$refs[formName].validate(valid => {
+        // 所有验证都通过
+        if (valid) {
+          // 拿数据
+          let params = {
+            account: this.editForm.account,
+            userType: this.editForm.userType,
+            id: this.editForm.id
+          };
+          // 发送数据
+          this.$http
+            .post("account/accounteditsave", params)
+            .then(res => {
+              // 重新请求数据
+              this.getAccountList();
+              // 隐藏模态框
+              this.dialogFormVisible = false;
+              // 提示消息
+              if (res.data.code === 0) {
+                this.$message({
+                  type: "success",
+                  message: res.data.message
+                });
+                return;
+              }
+              this.$message.error(res.data.message);
+              return;
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
+      });
     },
     //删除
     handleDelete(id) {
-      let params={
-        id
-      }
-      this.$http.get('account/accountdelete',params).then((res) => {
-        let {code,message}=res.data;
-        if(code===0){
+      this.$confirm("是否删除此用户?", "温馨提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          let params = {
+            id
+          };
+          this.$http
+            .get("account/accountdelete", params)
+            .then(res => {
+              let { code, message } = res.data;
+              if (code === 0) {
+                // 先刷新页面
+                this.getAccountList();
+                // 提示信息
+                this.$message({
+                  type: "success",
+                  message
+                });
+                return;
+              }
+              this.$message.error(message);
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        })
+        .catch(() => {
           this.$message({
-            type:'success',
-            message
+            type: "info",
+            message: "已取消"
           });
-          this.getAccountList();
-          return;
-        }
-        this.$message.error(message);
-      }).catch((err) => {
-        console.log(err);
-      });
+        });
+    },
+    // 执行批量删除
+    multipleDelete() {
+      // 没有选中任何一个
+      if (this.selectedIdArray.length === 0) {
+        this.$message({
+          type: "info",
+          message: "没有选中任何一项",
+          offset: 200
+        });
+        return;
+      }
+      this.$confirm("删除用户?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          let params = {
+            idArray: this.selectedIdArray
+          };
+          this.$http
+            .post("account/accountmultipledelete", params)
+            .then(res => {
+              let { code, message } = res.data;
+              if (code === 0) {
+                // 提示
+                this.$message({
+                  type: "success",
+                  message
+                });
+                // 刷新
+                this.getAccountList();
+                return;
+              }
+              this.$message.error(message);
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "取消删除",
+            offset: 200
+          });
+        });
+    },
+    // 监听选择变化
+    handleSelectionChange(val) {
+      // 取得选中项id
+      this.selectedIdArray = val.map(v => v.id);
+    },
+    // 清空选择状态
+    toggleSelection() {
+      this.$refs.accountManageTable.clearSelection();
     },
     // 分页相关
-    handleSizeChange() {},
-    handleCurrentChange() {}
+    // 每页条数
+    handleSizeChange(size) {
+      this.pageSize = size;
+      if (this.pageSize * this.currentPage < this.userTotal) {
+        // 重新拿数据
+        this.getAccountList();
+      }
+    },
+    // 当前页码
+    handleCurrentChange(page) {
+      this.currentPage = page;
+      // 重新拿数据
+      this.getAccountList();
+    }
   },
   // 钩子函数
   created() {
     this.getAccountList();
   },
+  // 过滤器
   filters: {
     formatDate(value) {
       // 格式化时间
